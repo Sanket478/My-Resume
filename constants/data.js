d4c397df250a192c342758371a60b04679cfe8ff
Danish Sheikh <danish.jnext@gmail.com>
2023-06-27 10:43:10 +0530

    Intial commit for Resume

---
+++ constants/data.js
@@ -0,0 +1,131 @@
+const userData = {
+  githubUsername: "danishsheikh0071",
+  name: "Danish Sheikh",
+  designation: "Jr. Reactjs Developer",
+  avatarUrl: "",
+  email: "danish@yopmail.com",
+  phone: "+91 9078XXXXXX",
+  address: "Bhavnagar, Gujarat, India.",
+  projects: [
+    // {
+    //   title: "React Quiz",
+    //   link: "https://tailwindmasterkit.com",
+    //   imgUrl: "/tmk.jpg",
+    // },
+    // {
+    //   title: "PlaceholderTech",
+    //   link: "https://placeholdertech.in",
+    //   imgUrl: "/placeholdertech.png",
+    // },
+    // {
+    //   title: "Portfolio",
+    //   link: "https://manuarora.in",
+    //   imgUrl: "/portfolio.png",
+    // },
+    // {
+    //   title: "CovidRescue",
+    //   link: "https://covidrescue.co.in",
+    //   imgUrl: "/covidrescue.png",
+    // },
+    // {
+    //   title: "Algochurn",
+    //   link: "https://algochurn.com",
+    //   imgUrl: "/algochurn.png",
+    // },
+    // {
+    //   title: "Playground",
+    //   link: "https://play.placeholdertech.in",
+    //   imgUrl: "/playground.png",
+    // },
+    // {
+    //   title: "Feedmeback",
+    //   link: "https://feedmeback.vercel.app",
+    //   imgUrl: "/feedmeback.png",
+    // },
+    // {
+    //   title: "Devmedium",
+    //   link: "https://devmedium.vercel.app",
+    //   imgUrl: "/devmedium.png",
+    // },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `I've been developing Reactjs Web Applications since the last 1 year. I didn't know what Web Development meant when I was a teenager. I have developed a hugh inetrest in Website Development, Problem Solving and Coding in general.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I've been coding in React, Bootstrap, Tailwind, C, and C++, programming language etc to name a few. The most help to me was myself (Psst, Stack overflow!), I consider myself a Rogue Ninja developer who is trying to learn everything.`,
+    ],
+    currentProject: "i - RMS",
+    currentProjectUrl: "",
+  },
+  experience: [
+    {
+      title: "Jr. Reactjs Developer",
+      company: "Jnext Technologies Pvt Ltd",
+      year: "Dec 2022 - Present",
+      companyLink: "",
+      desc: "I finally got what I always wanted to do, I now work with live projects, Develope and Design websites and manage the same..",
+    },
+    {
+      title: "Business Developer",
+      company: "Dev Satva",
+      year: "July 2021 - Dec 2022",
+      companyLink: "",
+      desc: "I started off with part-time then I get my-self a little time and joined full-time.",
+    },
+    {
+      title: "Jr Business Developer",
+      company: "NITSAN Technologies",
+      year: "Mar 2021 - Dec 2021",
+      companyLink: "",
+      desc: "I bring leads through various platforms, the projects are mainly for a CMS called TYPO3 and there are also other frameworks and technologies such as Laravel, Headless CMS, eCommerce stores, PWA, and much more. I have gained valuable experience in PR sheet making, email marketing, and content writing.",
+    },
+    {
+      title: "React Native Developer",
+      company: "Bytefury Bhavnagar",
+      year: "July 2020 - Jan 2021",
+      companyLink: "",
+      desc: "I worked as a React Native Developer where I learned React, HTML, CSS, and Javascript, I worked for 6 months which included 2 live projects named Crater and Safari.",
+    },
+    {
+      title: "iOS Developer Internship Ahmedabad",
+      company: "Zetrixweb LLP",
+      year: "Dec 2019 - Feb 2020 (3 Months)",
+      companyLink: "",
+      desc: "I did a 3-month internship before the pandemic and worked on several applications which require CRUD operations.",
+    },
+    {
+      title: "Business Developer",
+      company: "JD Consultancy Ahmedabad",
+      year: "Jun 2019 - Dec 2019",
+      companyLink: "",
+      desc: "I found my-self working for a consultancy firm which was connect to many IT companies at that time, I used to bring project leads.",
+    },
+    {
+      title: "Graduation",
+      company: "GMIT, Bhavnagar",
+      year: "2019",
+      companyLink: "",
+      desc: "Major in Information Technology and Engineering with a CGPA of 7.69. Nobody asks this but it's okay.",
+    },
+    {
+      title: "High School",
+      company: "Gyanmanjari vidyapith, Bhavnagar.",
+      year: "2015",
+      companyLink: "",
+      desc: "Subjects being the Science Stream - I barely survived with 56% marks.",
+    },
+  ],
+  resumeUrl: "",
+  socialLinks: {
+    instagram: "",
+    twitter: "",
+    linkedin: "https://www.linkedin.com/in/danish--sheikh/",
+    github: "https://github.com/danishsheikh0071",
+    facebook: "",
+    peerlist: "",
+  },
+};
+
+export default userData;
